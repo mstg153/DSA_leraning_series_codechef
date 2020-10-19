09f5b5f459bf68074eda72bee8bbf651123041e7
mstg153 <msaitejagoud153@gmail.com>
2020-10-19 11:35:26 +0530

    added cpp code to TEST problem

---
+++ Complexity_Analysis_+_Basics_Warm_Up/TEST.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+typedef long long ll;
+int main() {
+    while(1){
+        ll n;
+        cin >> n;
+        if(n==42)
+            return 0;
+        cout << n << endl;
+    }
+    return 0;
+}
